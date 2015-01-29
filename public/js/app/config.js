cb5d486b770d5de13a0612e0ee0088124c9fb293
cloudyan <1395093509@qq.com>
2015-01-29 20:17:10 +0800

    修改

--- public/js/app/config.js
+++ public/js/app/config.js
@@ -1,12 +1,12 @@
 app.config = {
   default_docs: ['css', 'dom', 'dom_events', 'html', 'http', 'javascript'],
-  docs_host: '<%= App.docs_host %>',
-  env: '<%= App.environment %>',
+  docs_host: App.docs_host,
+  env: App.environment,
   history_cache_size: 10,
-  index_path: '/<%= App.docs_prefix %>',
+  index_path: App.docs_prefix,
   max_results: 50,
-  production_host: 'devdocs.io',
+  production_host: '',
   search_param: 'q',
-  sentry_dsn: '<%= App.sentry_dsn %>',
-  version: '<%= Time.now.to_i %>'
+  sentry_dsn: App.sentry_dsn,
+  version: ''//Time.now.to_i
 };
