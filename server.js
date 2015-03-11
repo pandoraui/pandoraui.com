4b3df58cb542e649b81f08e0340439514bb76937
cloudyan <1395093509@qq.com>
2015-03-11 18:46:42 +0800

    更新

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var routes = require('./routes/router');
 
 // view engine setup
 app.set('port', process.env.PORT || 3000);
+
 app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -20,7 +21,7 @@ app.set('view engine', 'html');
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
